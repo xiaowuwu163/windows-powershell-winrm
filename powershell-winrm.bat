11c52e4a09eaf3f412c38825f3e45aff0139d52d
windows 7 <904826871@qq.com>
2019-02-28 11:03:53 +0800

    完善posershell-winrm.bat 执行脚本

--- powershell-winrm.bat
+++ powershell-winrm.bat
@@ -1,6 +1,7 @@
 powershell get-host
-set-ExecutionPolicy RemoteSigned
+powershell set-ExecutionPolicy RemoteSigned
 echo y | winrm quickconfig 
 winrm e winrm/config/listener
 winrm set winrm/config/service/auth @{Basic="true"}
 winrm set winrm/config/service @{AllowUnencrypted="true"}
+
