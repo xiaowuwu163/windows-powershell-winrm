f153e3535abf4a626bfa0a16a764f1aa387ec4c0
windows 7 <904826871@qq.com>
2019-03-05 16:00:11 +0800

    修改powershell 管道

--- powershell-winrm.bat
+++ powershell-winrm.bat
@@ -1,6 +1,6 @@
 powershell get-host
 powershell set-ExecutionPolicy RemoteSigned
-echo y | winrm quickconfig 
+echo y|winrm quickconfig 
 winrm e winrm/config/listener
 winrm set winrm/config/service/auth @{Basic="true"}
 winrm set winrm/config/service @{AllowUnencrypted="true"}
