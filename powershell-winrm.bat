9bab18dfeccd7144b1184c939c32c74b4df1c19b
windows 7 <904826871@qq.com>
2019-03-07 10:27:47 +0800

    增加SSL连接方式

--- powershell-winrm.bat
+++
@@ -1,7 +0,0 @@
-powershell get-host
-powershell set-ExecutionPolicy RemoteSigned
-echo y|winrm quickconfig 
-winrm e winrm/config/listener
-winrm set winrm/config/service/auth @{Basic="true"}
-winrm set winrm/config/service @{AllowUnencrypted="true"}
-
